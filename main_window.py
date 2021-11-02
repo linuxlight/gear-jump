dadea872ceceaa70998f3f43b3f8c402f53f693b
Seokjoo Cho <lnxlht4j@gmail.com>
2021-11-02 23:53:50 +0900

    added selected button background color

--- main_window.py
+++ main_window.py
@@ -2,12 +2,17 @@ import os
 import platform
 import sys
 
+
 if platform.system() == "Darwin":
     from PyQt6 import uic
+    from PyQt6.QtCore import Qt
     from PyQt6.QtWidgets import QMainWindow, QPushButton
+    from PyQt6.QtGui import QFont, QColor
 else:
     from PyQt5 import uic
+    from PyQt5.QtCore import Qt
     from PyQt5.QtWidgets import QMainWindow, QPushButton
+    from PyQt5.QtGui import QFont, QColor
 
 from app import MainApp
 from gear import Gear
@@ -39,8 +44,20 @@ class MainWindow(QMainWindow):
         target_gear = self.app.select_gear(target_stage)
         self.app.set_current_gear(target_gear)
         self.set_gear_display(target_gear)
+        self.set_color(target_stage)
         print("[선택된 기어]", target_gear)
         print()
+
+    def set_color(self, selected):
+        for button in self.buttons:
+            if button.objectName() == f"gearGroup_{selected}":
+                pal = button.palette()
+                pal.setColor(button.backgroundRole(), QColor(Qt.blue))
+                button.setPalette(pal)
+            else:
+                button.setPalette(QPushButton().palette())
+            button.setAutoFillBackground(True)
+            button.update()
 
     def set_gear_display(self, target_gear: Gear):
         self.frontGear.setText(str(target_gear.front_idx + 1))
@@ -57,8 +74,10 @@ class MainWindow(QMainWindow):
             row = self.gearGridLayout.rowCount() + 1
             col = 0
         new_group = QPushButton(self)
+        new_group.setObjectName(f"gearGroup_{stage}")
         new_group.setText(str(stage))
         new_group.setMinimumSize(0, 100)
+        new_group.setFont(QFont("Arial", 15))
         new_group.clicked.connect(lambda: self.change_gear(stage))
         self.buttons.append(new_group)
         self.gearGridLayout.addWidget(new_group, row-1, col)
